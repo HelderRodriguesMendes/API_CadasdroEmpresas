31cda3ebb8b260c6d654161a138fd86453f04820
Helder Rodrigues Mendes <helderrodrigues37@gmail.com>
2020-11-01 10:05:14 -0300

    alterando estado, cidade e vizinhança de uma company

--- src/main/java/br/com/testePratico/model/State.java
+++ src/main/java/br/com/testePratico/model/State.java
@@ -11,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,6 +33,7 @@ public class State implements Serializable { // ESTADO
 	@Size(min = 2, max = 30)
 	private String name;
 	
+	@JsonBackReference
 	@ManyToOne()
 	@JoinColumn(name = "COUNTRY_ID")
 	private Country country;
